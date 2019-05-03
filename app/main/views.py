00456c53a6854605d294474992c6da6fd2faf662
Tim Lanzi <trlanzi@gmail.com>
2019-05-02 22:23:05 -0400

    Building out the login functionality

--- app/main/views.py
+++ app/main/views.py
@@ -1,31 +1,28 @@
-from datetime import datetime
-from flask import render_template, session, redirect, url_for
-from . import main
-from . forms import NameForm
+from flask import render_template, session, redirect, url_for, current_app
 from .. import db
 from ..models import User
+from ..email import send_email
+from . import main
+from . forms import NameForm
+
 
 @main.route('/', methods=['GET', 'POST'])
 def index():
   form = NameForm()
   if form.validate_on_submit():
-    # old_name = session.get('name')
     user = User.query.filter_by(username=form.name.data).first()
     if user is None:
       user = User(username=form.name.data)
       db.session.add(user)
-      #db.session.commit()
+      db.session.commit()
       session['known'] = False
-      if app.config['FLASKY_ADMIN']:
-        send_email(app.config['FLASKY_ADMIN'], 'New User', 'mail/new_user', user=user)
+      if current_app.config['FLASKY_ADMIN']:
+        send_email(current_app.config['FLASKY_ADMIN'], 'New User', 'mail/new_user', user=user)
     else:
       session['known'] = True
     session['name'] = form.name.data
-    form.name.data = ''
     return redirect(url_for('.index'))
   return render_template('index.html', 
     form=form,
     name=session.get('name'),
-    known=session.get('known', False),
-    current_time=datetime.utcnow()
-  )
+    known=session.get('known', False))
