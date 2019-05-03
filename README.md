ab6328739ce5dd9c96abc5153d9200d5032e5c9a
Tim Lanzi <trlanzi@gmail.com>
2019-05-02 20:51:00 -0400

    Restructured the app in chapter 7

--- README.md
+++ README.md
@@ -2,3 +2,5 @@
 Making my first Flask app using the O'Reilly book "Flask Web Development"
 
 To start venv, use source venv/bin/activate (venv/Scripts/activate on Windows).
+
+Set FLASK_APP env variable with export FLASK_APP=hello.py
--- hello.py
+++ hello.py
@@ -1,3 +1,5 @@
+import os
+from threading import Thread
 from flask import Flask, render_template, session, redirect, url_for, flash
 from flask_bootstrap import Bootstrap
 from flask_moment import Moment
@@ -6,37 +8,42 @@ from flask_wtf import FlaskForm
 from wtforms import StringField, SubmitField
 from wtforms.validators import DataRequired
 
+app = Flask(__name__)
+
+bootstrap = Bootstrap(app)
+moment = Moment(app)
+
+
 class NameForm(FlaskForm):
   name = StringField('What is your name?', validators=[DataRequired()])
   submit = SubmitField('Submit')
 
-app = Flask(__name__)
-app.config['SECRET_KEY'] = 'hard to guess sting'
-bootstrap = Bootstrap(app)
-moment = Moment(app)
 
 @app.route('/', methods=['GET', 'POST'])
 def index():
   form = NameForm()
   if form.validate_on_submit():
-    old_name = session.get('name')
-    if old_name is not None and old_name != form.name.data:
-      flash('Looks like you have changed your name!')
+    # old_name = session.get('name')
+    user = User.query.filter_by(username=form.name.data).first()
+    if user is None:
+      user = User(username=form.name.data)
+      db.session.add(user)
+      #db.session.commit()
+      session['known'] = False
+      if app.config['FLASKY_ADMIN']:
+        send_email(app.config['FLASKY_ADMIN'], 'New User', 'mail/new_user', user=user)
+    else:
+      session['known'] = True
     session['name'] = form.name.data
+    form.name.data = ''
     return redirect(url_for('index'))
-  return render_template('index.html', form=form, name=session.get('name'))
+  return render_template('index.html', 
+    form=form,
+    name=session.get('name'),
+    known=session.get('known', False)
+  )
 
 
 @app.route('/user/<name>')
 def user(name):
   return render_template('user.html', name=name)
-
-
-@app.errorhandler(404)
-def page_not_found(e):
-  return render_template('404.html'), 404
-
-
-@app.errorhandler(500)
-def internal_server_error(e):
-  return render_template('500.html'), 500
