d3e08b539119c8d8c190130ec8f48c09e2e2fa7b
Tim Lanzi <trlanzi@gmail.com>
2019-05-06 23:03:54 -0400

    completed chapter 12

--- app/models.py
+++ app/models.py
@@ -106,6 +106,7 @@ class User(UserMixin, db.Model):
 
   def __init__(self, **kwargs):
     super(User, self).__init__(**kwargs)
+    self.follow(self)
     if self.role is None:
       if self.email == current_app.config['FLASKY_ADMIN']:
         self.role = Role.query.filter_by(name='Administrator').first()
@@ -122,6 +123,14 @@ class User(UserMixin, db.Model):
   def followed_posts(self):
     return Post.query.join(Follow, Follow.followed_id == Post.author_id)\
       .filter(Follow.follower_id == self.id)
+
+  @staticmethod
+  def add_self_follows():
+    for user in User.query.all():
+      if not user.is_following(user):
+        user.follow(user)
+        db.session.add(user)
+        db.session.commit()
 
   @password.setter
   def password(self, password):
